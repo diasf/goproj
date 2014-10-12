e83af00675f4475b5f9bfa72fbdf1743c441ac4e
Filipe Dias <f.dias@outlook.com>
2014-10-12 12:57:05 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
 goproj
 ======
 
-Bash function to switch between Go projects
+Bash function to switch between Go projects.
+
+# Configuration
+
+1. Clone it, somewhere
+
+
+```
+cd ~/scripts
+git clone https://github.com/diasf/goproj
+```
+
+2. Include it, in your .bashrc if you like, and add your projects to the GOPROJECTS bash array pointing to their location:
+
+
+```
+. ~/scripts/goproj/goproj.sh
+
+# mapping of goproject to location
+GOPROJECTS[kv]=~/go/src/github.com/cznic/kv
+GOPROJECTS[camlistore]=~/proj/camlistore/src/camlistore.org
+GOPROJECTS[mgo]=~/go/src/github.com/go-mgo/mgo
+```
+
+3. Use it
+
+
+Now you can type goproj followed by the projects key in the array to go to the projects directory and set the GOPATH variable:
+
+```
+goproj camlistore
+```
+
+The function supports autocompletion, so when typing `goproj c` and `TAB` it will autocomplete to `goproj camlistore`.
+
+
+# Options
+
+* -v: Open vim in the project directory
---
+++ goproj.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+declare -A GOPROJECTS
+
+# ------------------------------------------------------------------------
+# the main goproj command
+# ------------------------------------------------------------------------
+function goproj() {
+    VIM_OPT=false
+
+    while getopts ":v" opt; do
+        case $opt in
+            v)
+                VIM_OPT=true
+                ;;
+            \?)
+                echo "Invalid option: -$OPTARG" >&2
+                ;;
+        esac
+        shift $(($OPTIND-1))
+    done
+
+    cd ${GOPROJECTS[$1]}
+    _setGOPATH
+
+    if [ "$VIM_OPT" = true ]; then
+        vim
+    fi
+}
+
+# ------------------------------------------------------------------------
+# autocomplition for the goproj command line
+# ------------------------------------------------------------------------
+_goproj() {
+    cur=${COMP_WORDS[COMP_CWORD]}
+    for project in "${!GOPROJECTS[@]}"
+    do
+        if [[ $project == $cur* ]]; then
+            use="$use $project"
+        fi
+    done
+    COMPREPLY=( $( compgen -W "$use" -- $cur ) )
+}
+complete -o default -o nospace -F _goproj goproj 
+
+# ------------------------------------------------------------------------
+# Sets the GOPATH variable to the directory above src, by going up the
+# filesystem
+# ------------------------------------------------------------------------
+function _setGOPATH() {
+    local originalPath=`pwd`
+    local current=
+    while true; do
+        current=$(basename "$PWD")
+        if [[ "$current" = "src" ]]; then
+            cd ..
+            echo "Setting GOPATH to: `pwd`"
+            export GOPATH=`pwd`
+            break
+        elif [[ "$current" = "/" ]]; then
+            echo "GOPATH not found"
+            break
+        fi
+        cd ..
+	done
+    cd $originalPath
+}
